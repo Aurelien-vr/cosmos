53b67c4aca5d701b63a3b0181cbbe7e38ee05e09
Mallart <mallart@mallart.lan>
2026-02-08 01:19:30 +0100

    actually removed all garbage from .cache/ and .build/ folders

--- Readme.md
+++
@@ -1,3 +0,0 @@
-# Cosmos
-
-**C**elestial **O**bject **S**imulation and **M**ovement **O**ptimization **S**ystem is a project made using OpenGL.
--- src/auVector2.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct auVector2 {
-  float x, y;
-
-  auVector2() : x(0.f), y(0.f) {};
-  auVector2(float x, float y) : x(x), y(y) {};
-};
